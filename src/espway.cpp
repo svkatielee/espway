4337e162a3497e5e1426f86012c232e0284caec7
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-01-04 14:41:03 +0200

    Add wifi config, OTA

--- src/espway.cpp
+++ src/espway.cpp
@@ -2,6 +2,9 @@
 #include <Hash.h>
 #include <Wire.h>
 
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+
 #include <I2Cdev.h>
 #include <MPU6050.h>
 
@@ -14,7 +17,7 @@ MPU6050 mpu;
 int16_t ax, ay, az, gx, gy, gz;
 
 const bool logFrequency = true;
-const int nSamples = 100;
+const int nSamples = 1000;
 unsigned long lastTime = 0;
 int sampleCounter = 0;
 
@@ -27,24 +30,34 @@ void mpuInterrupt() {
 void setup() {
     Serial.begin(115200);
 
+    // I2C initialization
     Wire.begin(0, 5);
     Wire.setClock(400000);
-
+    // MPU6050 initialization
     mpu.initialize();
-    mpu.setRate(1);
+    mpu.setRate(0);
     mpu.setDLPFMode(MPU6050_DLPF_BW_20);
     mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
     mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_2000);
-    mpu.setIntEnabled(true);
+    mpu.setIntDataReadyEnabled(true);
     mpu.setInterruptMode(MPU6050_INTMODE_ACTIVELOW);
     mpu.setInterruptDrive(MPU6050_INTDRV_PUSHPULL);
-
+    mpu.setIntEnabled(true);
     attachInterrupt(4, mpuInterrupt, RISING);
+
+    // WiFi soft AP init
+    WiFi.softAP("ESPway");
+    // ArduinoOTA init
+    ArduinoOTA.begin();
 }
 
 
 void loop() {
-    while (!intFlag);
+    if (!intFlag) {
+        ArduinoOTA.handle();
+        return;
+    }
+
     intFlag = false;
     mpu.getIntStatus();
 
