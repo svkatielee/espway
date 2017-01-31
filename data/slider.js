4daef4ab5ef9e239570fe3e6a586e5486444fb0c
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-02-01 00:23:05 +0200

    Continuing on slider implementation

--- data/slider.js
+++ data/slider.js
@@ -1,17 +1,44 @@
-<slider label='{ opts.label }'>
+<slider label={ opts.label }>
     <div class='sliderInnerContainer'>
         <div>{ opts.label } = <span>{ mantissa }</span> × 10<sup>{ exponent }</sup></div>
         <div>
-            <button>Center</button>
+            <button onclick={ centerclick }>Center</button>
             <button>Reset</button>
         </div>
     </div>
 
     <div class='sliderInnerContainer'>
-        <div class='bound left'><span class='times' show={ centered }>× </span>10<sup>1</sup></div>
-        <input type='range' />
-        <div class='bound right'><span class='times' show={ centered }>× </span>10<sup>5</sup></div>
+        <div class='bound left'>
+            <span class='times' show={ centered }>× </span>10<sup>{ boundMin }</sup>
+        </div>
+        <input ref='rangeslider' type='range' oninput={ sliderChange }
+            min={ boundMin } max={ boundMax} step={ step }/>
+        <div class='bound right'>
+            <span class='times' show={ centered }>× </span>10<sup>{ boundMax }</sup>
+        </div>
     </div>
+
+    let refs = this.refs
+
+    function resetSlider(boundMin, boundMax) {
+        refs.rangeslider.min = boundMin
+        refs.rangeslider.max = boundMax
+        refs.rangeslider.step = (boundMax - boundMin) / 100
+    }
+
+    sliderChange(e) {
+        let val = Math.pow(10, this.refs.rangeslider.value)
+        let exponent = Math.floor(this.refs.rangeslider.value)
+        let mantissa = val / Math.pow(10, exponent)
+        this.mantissa = mantissa.toFixed(1)
+        this.exponent = exponent
+    }
+
+    this.boundMin = -2
+    this.boundMax = 3
+    this.step = 5 / 100
+    this.centered = false
+    this.centervalue = 0
 
     <style>
         .sliderInnerContainer > input {
