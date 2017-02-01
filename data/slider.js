dd2fe271e719dd764b258d653aa6cce7674ae742
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2017-02-01 20:27:10 +0200

    Slider component now roughly working

--- data/slider.js
+++ data/slider.js
@@ -2,8 +2,8 @@
     <div class='sliderInnerContainer'>
         <div>{ opts.label } = <span>{ mantissa }</span> × 10<sup>{ exponent }</sup></div>
         <div>
-            <button onclick={ centerclick }>Center</button>
-            <button>Reset</button>
+            <button onclick={ centerClick }>Center</button>
+            <button onclick={ resetClick }>Reset</button>
         </div>
     </div>
 
@@ -11,34 +11,61 @@
         <div class='bound left'>
             <span class='times' show={ centered }>× </span>10<sup>{ boundMin }</sup>
         </div>
-        <input ref='rangeslider' type='range' oninput={ sliderChange }
-            min={ boundMin } max={ boundMax} step={ step }/>
+        <input type='range' ref='slider' oninput={ sliderChange } value={ sliderValue }
+            min={ boundMin } max={ boundMax } step={ step } />
         <div class='bound right'>
             <span class='times' show={ centered }>× </span>10<sup>{ boundMax }</sup>
         </div>
     </div>
 
-    let refs = this.refs
+    function log10(x) {
+        return Math.log(x) * Math.LOG10E;
+    }
 
-    function resetSlider(boundMin, boundMax) {
-        refs.rangeslider.min = boundMin
-        refs.rangeslider.max = boundMax
-        refs.rangeslider.step = (boundMax - boundMin) / 100
+    setBounds(boundMin, boundMax) {
+        this.boundMin = boundMin
+        this.boundMax = boundMax
+        this.step = (boundMax - boundMin) / 100
     }
 
     sliderChange(e) {
-        let val = Math.pow(10, this.refs.rangeslider.value)
-        let exponent = Math.floor(this.refs.rangeslider.value)
-        let mantissa = val / Math.pow(10, exponent)
+        this.refreshValue()
+    }
+
+    refreshValue() {
+        let val = this.refs.slider.value
+        let expval = Math.pow(10, val)
+        if (this.centered) {
+            expval *= this.centerValue
+        }
+        let exponent = Math.floor(log10(expval))
+        let mantissa = expval / Math.pow(10, exponent)
         this.mantissa = mantissa.toFixed(1)
         this.exponent = exponent
     }
 
-    this.boundMin = -2
-    this.boundMax = 3
-    this.step = 5 / 100
+    centerClick(e) {
+        this.centered = true
+        let val = this.refs.slider.value
+        this.centerValue = Math.pow(10, val)
+        this.setBounds(-1, 1)
+        this.refs.slider.value = 0
+    }
+
+    resetClick(e) {
+        this.centered = false
+        this.setBounds(-2, 3)
+    }
+
     this.centered = false
-    this.centervalue = 0
+    this.centerValue = 0
+    this.setBounds(-2, 3)
+
+    this.on('mount', () => {
+        this.refs.slider.value = this.boundMin
+        this.refreshValue()
+        this.update()
+    })
 
     <style>
         .sliderInnerContainer > input {
@@ -56,7 +83,7 @@
         }
 
         .bound {
-            width: 4.5em;
+            width: 5em;
         }
 
         .right {
